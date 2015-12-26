31156e9b64acce83469a84f72f550a2b4c701ed4
Kleber Mota <klebermo@gmail.com>
2015-12-26 11:59:25 -0300

    Application.java

--- src/main/java/org/hello/Application.java
+++ src/main/java/org/hello/Application.java
@@ -1,9 +1,12 @@
 package org.hello;
 
+import org.hello.custom.thymeleaf.dialect.FormDialect;
+import org.hello.custom.thymeleaf.dialect.NavDialect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
@@ -16,5 +19,15 @@ public class Application extends SpringBootServletInitializer {
 	public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
+	
+	@Bean
+	public FormDialect formDialect() {
+		return new FormDialect();
+	}
+	
+	@Bean
+	public NavDialect navDialect() {
+		return new NavDialect();
+	}
 	
 }
