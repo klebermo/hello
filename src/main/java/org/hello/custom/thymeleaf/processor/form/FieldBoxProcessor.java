5e693565e36c4b2b78201a09ec5e56007936903e
Kleber Mota <klebermo@gmail.com>
2015-12-27 12:59:25 -0300

    FieldBoxProcessor

---
+++ src/main/java/org/hello/custom/thymeleaf/processor/form/FieldBoxProcessor.java
@@ -0,0 +1,45 @@
+package org.hello.custom.thymeleaf.processor.form;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+
+import org.thymeleaf.Arguments;
+import org.thymeleaf.dom.Element;
+import org.thymeleaf.dom.Node;
+import org.thymeleaf.processor.ProcessorResult;
+import org.thymeleaf.processor.element.AbstractElementProcessor;
+
+public class FieldBoxProcessor extends AbstractElementProcessor {
+
+	public FieldBoxProcessor() {
+		super("field-box");
+	}
+
+	@Override
+	protected ProcessorResult processElement(Arguments arguments, Element element) {
+		String type = element.getAttributeValue("type");
+		Field field = (Field) arguments.getLocalVariable("field");
+		
+		for(Annotation annotation : field.getAnnotations()) {
+			if(annotation.annotationType().getSimpleName().equals(type)) {
+				Element fieldbox = new Element("field-box");
+				
+				for(Node child : element.getChildren()) {
+					child.setProcessable(true);
+					fieldbox.addChild(child);
+				}
+				
+				element.getParent().insertBefore(element, fieldbox);
+				element.getParent().removeChild(element);
+			}
+		}
+		
+		return ProcessorResult.OK;
+	}
+
+	@Override
+	public int getPrecedence() {
+		return 0;
+	}
+
+}
