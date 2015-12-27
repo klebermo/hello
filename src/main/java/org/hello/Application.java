2e8226bf61443b11377752da996528c47487d430
Kleber Mota <klebermo@gmail.com>
2015-12-27 12:59:44 -0300

    classe Application

--- src/main/java/org/hello/Application.java
+++ src/main/java/org/hello/Application.java
@@ -1,12 +1,19 @@
 package org.hello;
 
+import java.lang.annotation.Annotation;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.hello.custom.thymeleaf.dialect.FormDialect;
 import org.hello.custom.thymeleaf.dialect.NavDialect;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
+import org.springframework.core.type.filter.AnnotationTypeFilter;
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
@@ -28,6 +35,20 @@ public class Application extends SpringBootServletInitializer {
 	@Bean
 	public NavDialect navDialect() {
 		return new NavDialect();
+	}
+	
+	public static List<Class<?>> getListaClasses(String target) throws ClassNotFoundException {
+		List<Class<?>> lista = new ArrayList<Class<?>>();
+		
+		ClassPathScanningCandidateComponentProvider scanner = new ClassPathScanningCandidateComponentProvider(false);
+		Class<? extends Annotation> annotation = (Class<? extends Annotation>) Class.forName("org.hello.custom.annotation.nav." + target);
+		scanner.addIncludeFilter(new AnnotationTypeFilter(annotation));
+		for (BeanDefinition bd : scanner.findCandidateComponents("com.spring.loja.model")) {
+			Class<?> clazz = Class.forName(bd.getBeanClassName());
+			lista.add(clazz);
+		}
+		
+		return lista;
 	}
 	
 }
