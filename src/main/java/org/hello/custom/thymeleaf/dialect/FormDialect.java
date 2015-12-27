4cd4e47661ce6033ccbc1854cc452e8b89d67a64
Kleber Mota <klebermo@gmail.com>
2015-12-27 15:54:49 -0300

    FormDialect

--- src/main/java/org/hello/custom/thymeleaf/dialect/FormDialect.java
+++ src/main/java/org/hello/custom/thymeleaf/dialect/FormDialect.java
@@ -3,6 +3,12 @@ package org.hello.custom.thymeleaf.dialect;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.hello.custom.thymeleaf.processor.field.CheckboxProcessor;
+import org.hello.custom.thymeleaf.processor.field.InputProcessor;
+import org.hello.custom.thymeleaf.processor.field.LabelProcessor;
+import org.hello.custom.thymeleaf.processor.field.RadioProcessor;
+import org.hello.custom.thymeleaf.processor.field.SelectProcessor;
+import org.hello.custom.thymeleaf.processor.field.TextareaProcessor;
 import org.hello.custom.thymeleaf.processor.form.FieldBoxProcessor;
 import org.hello.custom.thymeleaf.processor.form.FormProcessor;
 import org.thymeleaf.dialect.AbstractDialect;
@@ -18,6 +24,12 @@ public class FormDialect extends AbstractDialect {
 		final Set<IProcessor> processor = new HashSet<IProcessor>();
 		processor.add(new FormProcessor());
 		processor.add(new FieldBoxProcessor());
+		processor.add(new CheckboxProcessor());
+		processor.add(new InputProcessor());
+		processor.add(new LabelProcessor());
+		processor.add(new RadioProcessor());
+		processor.add(new SelectProcessor());
+		processor.add(new TextareaProcessor());
 		return processor;
 	}
 
