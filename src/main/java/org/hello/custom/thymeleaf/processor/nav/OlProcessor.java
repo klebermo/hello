0738e2e8234e1c646485732004773b649ae4b60e
Kleber Mota <klebermo@gmail.com>
2015-12-27 12:58:32 -0300

    processor Ol/Ul

--- src/main/java/org/hello/custom/thymeleaf/processor/nav/OlProcessor.java
+++ src/main/java/org/hello/custom/thymeleaf/processor/nav/OlProcessor.java
@@ -2,6 +2,7 @@ package org.hello.custom.thymeleaf.processor.nav;
 
 import org.thymeleaf.Arguments;
 import org.thymeleaf.dom.Element;
+import org.thymeleaf.dom.Text;
 import org.thymeleaf.processor.ProcessorResult;
 import org.thymeleaf.processor.element.AbstractElementProcessor;
 
@@ -13,6 +14,27 @@ public class OlProcessor extends AbstractElementProcessor {
 
 	@Override
 	public ProcessorResult processElement(Arguments arguments, Element element) {
+		String target = element.getAttributeValue("target");
+		
+		Element ol = new Element("ol");
+		ol.setProcessable(true);
+		try {
+			for(Class<?> clazz : org.hello.Application.getListaClasses(target)) {
+				Element li = new Element("li");
+				
+				Element a = new Element("a");
+				a.setAttribute("href", "#");
+				a.addChild(new Text(clazz.getSimpleName()));
+				
+				li.addChild(a);
+				ol.addChild(li);
+			}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		element.getParent().insertBefore(element, ol);
+		element.getParent().removeChild(element);
+		
 		return ProcessorResult.OK;
 	}
 
