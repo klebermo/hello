7ee95ad21f4a0c52c86a869dd0e0ca0408c292c5
Kleber Mota <klebermo@gmail.com>
2015-12-27 12:59:15 -0300

    FormProcessor

--- src/main/java/org/hello/custom/thymeleaf/processor/form/FormProcessor.java
+++ src/main/java/org/hello/custom/thymeleaf/processor/form/FormProcessor.java
@@ -1,7 +1,10 @@
 package org.hello.custom.thymeleaf.processor.form;
 
+import java.lang.reflect.Field;
+
 import org.thymeleaf.Arguments;
 import org.thymeleaf.dom.Element;
+import org.thymeleaf.dom.Node;
 import org.thymeleaf.processor.ProcessorResult;
 import org.thymeleaf.processor.element.AbstractElementProcessor;
 
@@ -13,6 +16,26 @@ public class FormProcessor extends AbstractElementProcessor {
 
 	@Override
 	public ProcessorResult processElement(Arguments arguments, Element element) {
+		String classe = element.getAttributeValue("class");
+		
+		Element form = new Element("form");
+		
+		try {
+			Class<?> clazz = Class.forName("org.hello.model." + classe.toLowerCase() + "." + classe);
+			for(Field field : clazz.getFields()) {
+				for(Node child : element.getChildren()) {
+					child.setNodeLocalVariable("field", field);
+					child.setProcessable(true);
+					form.addChild(child);
+				}
+			}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		element.getParent().insertBefore(element, form);
+		element.getParent().removeChild(element);
+		
 		return ProcessorResult.OK;
 	}
 
