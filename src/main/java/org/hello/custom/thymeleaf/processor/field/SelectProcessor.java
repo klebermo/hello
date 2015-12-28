00910e0b227f398996b37f01add98f929a11b08c
Kleber Mota <klebermo@gmail.com>
2015-12-28 10:29:39 -0300

    SelectProcessor

--- src/main/java/org/hello/custom/thymeleaf/processor/field/SelectProcessor.java
+++ src/main/java/org/hello/custom/thymeleaf/processor/field/SelectProcessor.java
@@ -1,6 +1,7 @@
 package org.hello.custom.thymeleaf.processor.field;
 
 import java.lang.reflect.Field;
+import java.util.Collection;
 import java.util.Locale;
 import java.util.Map;
 import java.util.ResourceBundle;
@@ -14,8 +15,6 @@ import org.thymeleaf.dom.Element;
 import org.thymeleaf.dom.Text;
 import org.thymeleaf.processor.ProcessorResult;
 import org.thymeleaf.processor.element.AbstractElementProcessor;
-
-import antlr.collections.List;
 
 public class SelectProcessor extends AbstractElementProcessor {
 
@@ -32,7 +31,7 @@ public class SelectProcessor extends AbstractElementProcessor {
 		Element node = new Element("select");
 		node.setAttribute("name", field.getName());
 		
-		if(field.getType().equals(List.class))
+		if(field.getType().equals(Collection.class))
 			node.setAttribute("multiple", "multiple");
 		
 		for( Map.Entry<String, Attribute> entry : element.getAttributeMap().entrySet() )
@@ -50,7 +49,9 @@ public class SelectProcessor extends AbstractElementProcessor {
 			}
 		} else {
 			try {
-				Class<?> clazz = Class.forName("org.hello.model." + field.getName() + "." + field.getName() + "Dao");
+				String field_name = field.getName();
+				String class_name = field_name.substring(0, 1).toUpperCase() + field_name.substring(1, field_name.length());
+				Class<?> clazz = Class.forName("org.hello.model." + field_name + "." + class_name + "Dao");
 				Dao dao = (Dao) clazz.newInstance();
 				org.hello.ApplicationContextHolder.getContext().getAutowireCapableBeanFactory().autowireBean(dao);
 				for(Object object : dao.select()) {
